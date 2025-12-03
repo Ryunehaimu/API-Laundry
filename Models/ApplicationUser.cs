927a580ae08cb163fee8a0166a75f3952041ab5c
Dede Rahmat F <108001133+Ryunehaimu@users.noreply.github.com>
2025-12-03 12:00:55 +0700

    first commit

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace SamLaundry.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Example: public string FullName { get; set; }
+        
+    }
+}
